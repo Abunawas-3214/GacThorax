73b399718025ff5cd7cf770e314eba6f0bf0aa6a
Abunawas <abunawas.arifandi@gmail.com>
2022-08-04 22:06:39 +0700

    add import app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template
+import os
+import shutil
+import ProgramUtama
 
 app = Flask(__name__)
 
