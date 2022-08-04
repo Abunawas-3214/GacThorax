93810a4e6fee48882ca55617f582ec2bdec070c5
Abunawas <abunawas.arifandi@gmail.com>
2022-08-04 21:58:30 +0700

    fix app.py

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 def delete_files():
     for filename in os.listdir(UPLOAD_FOLDER):
