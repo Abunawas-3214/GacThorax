c5d5df0ef798005e04cab0a9e90fc13d10d0135d
Abunawas <abunawas.arifandi@gmail.com>
2022-09-29 00:45:35 +0700

    Fix

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
